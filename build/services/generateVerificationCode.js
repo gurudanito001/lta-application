aee8199877e55cbd31bcb0ec8fbb6a89b2566011
gurudanito001 <youareontodan@gmail.com>
2024-06-29 05:45:06 +0100

    after prisma generate

--- build/services/generateVerificationCode.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateRandomCode = generateRandomCode;
-function generateRandomCode() {
-    // Generate a random number between 1000 and 9999 (inclusive)
-    const code = Math.floor(Math.random() * 9000) + 1000;
-    return code;
-}
